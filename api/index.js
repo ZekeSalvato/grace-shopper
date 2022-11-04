97aaa4e3543df61bea152fec9d0f10dadea9c522
AlphonseCoco <alphonsecoco@gmail.com>
2022-11-03 20:38:01 -0500

    changes to db seed, fixed CONFLCIT, added price to products table, added createuser req and admin

--- api/index.js
+++ api/index.js
@@ -2,7 +2,14 @@ const express = require('express');
 
 const apiRouter = express.Router();
 
+const usersRouter = require('./usersRouter');
+apiRouter.use('/users', usersRouter)
+
 const productsRouter = require('./productsRouter');
 apiRouter.use('/products', productsRouter);
+
+const cartRouter = require('./cartRouter');
+apiRouter.use('/cart', cartRouter)
+
 
 module.exports = apiRouter;
