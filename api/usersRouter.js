a9b07558afa1eb11a3722b5f64e025142aa92c1c
zekesalvato <zsalvato71@gmail.com>
2022-11-05 09:47:10 -0500

    create initial users

--- api/usersRouter.js
+++ api/usersRouter.js
@@ -3,7 +3,7 @@ const express = require('express');
 const usersRouter = express.Router();
 
 usersRouter.get('/', (req, res, next) => {
-  res.send('LIST OF USERS')
+  res.send('LIST OF USERS', users)
 })
 
 module.exports = usersRouter;
