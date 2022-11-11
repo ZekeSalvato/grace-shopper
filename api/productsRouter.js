666a3842f87c75a108812e6fa1f1e10b24ffa82c
AlphonseCoco <alphonsecoco@gmail.com>
2022-11-10 18:52:33 -0600

    fleshed out productsrouter

--- api/productsRouter.js
+++ api/productsRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { fetchAllProducts } = require('../db/products')
+const { fetchAllProducts, getProductById } = require('../db/products')
 const productsRouter = express.Router();
 
 const { createProduct, updateProduct, deleteProduct } = require('../db');
@@ -16,16 +16,53 @@ productsRouter.get('/', async (req, res, next) => {
     throw error
   }
 })
-// productsRouter.get('/', async (req, res, next) => {
-//   try {
-//     console.log("above")
-//       const products = await fetchAllProducts();
-//       console.log("below", products)
-//       res.send(products);
-//   } catch (error) {
-//       next(error)
-//   }
-// })
+// remember to add requireAdmin to post
+productsRouter.post('/', async (req, res, next) => {
+  const { title, description, price } = req.body;
+  try {
+    const product = await createProduct(title, description, price);
+    res.send({ product });
+  } catch (error) {
+    console.log("Couldn't post products")
+    throw error
+  }
+})
+// requireAdmin
+productsRouter.patch('/:id', async (req, res, next) => {
+  const { productId } = req.params; 
+  const { title, description, price} = req.body;
+  try {
+    const product = await getProductById(productId);
+    const updatedProduct = await updateProduct({productId, title, description, price });
+      res.send(updatedProduct)
+  } catch(error){
+    console.log("Couldn't udpate product")
+    throw error
+  }})
+
+// requireAdmin
+productsRouter.delete('/:id', async (req, res, next) => {
+  if (!req.user.isAdmin){
+    res.send({error: "Not Authorized for user"});
+    return;
+  }
+  const { productId } = req.params
+  try {
+    const deletedProduct = await deleteProduct(productId)
+    return deletedProduct;
+  } catch (error) {
+    console.log("couldn't delete product")
+    throw error
+  }
+})
+
+
+
+
+
+
+
+
 
 
 module.exports = productsRouter;
