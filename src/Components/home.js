4a7b3c0f38cc19177e62761b79dd15a9dde31d0d
spootscodes <passmanscout@icloud.com>
2022-11-15 18:31:20 -0600

    css file stuff

---
+++ src/Components/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './CSS/home.css';
+
+const Home = () => {
+    return (
+        <div id="homePage">
+            <h1 id="siteTitle">Flashback to the 90's </h1>
+        </div>
+    );
+}
+
+export default Home;
