08175cb50b4ccd7347b5a8c75a3f63ce7f61012d
spootscodes <passmanscout@icloud.com>
2022-11-22 20:54:25 -0600

    simple cleanup of home/added background

--- src/Components/home.js
+++ src/Components/home.js
@@ -5,8 +5,7 @@ import './CSS/Home.css';
 const Home = () => {
     return (
         <div id="homePage">
-            <h1 id="siteTitle">90's-Flashback.fm </h1>
-            <p>Tune in to relive the toys & electronics the 90's had to offer</p>
+            <h1 id="siteTitle">90's Flashback.fm </h1>
         </div>
     );
 }
