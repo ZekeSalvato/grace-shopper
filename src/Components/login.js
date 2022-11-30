63c70f1487fdf8aebf6af951f8899eb3877f7367
spootscodes <passmanscout@icloud.com>
2022-11-29 19:18:59 -0600

    changes to login component

--- src/Components/login.js
+++ src/Components/login.js
@@ -1,38 +1,42 @@
-import React, {useState} from 'react';
+import { React, useState } from 'react';
 import { logInUser } from '../api';
-import { useNavigate  } from 'react-router-dom';
+import { useNavigate, Link  } from 'react-router-dom';
 
-const Login = ({ setToken }) => {
+const Login = (props) => {
+    const { setToken } = props;
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
 
     const handleSubmit = async () => {
         const results = await logInUser(username, password);
-        if (results.token) {
-            setToken(results.token);
-            window.localStorage.setItem('token', results.token);
-            navigate('/')
+        if (results.success) {
+            setToken(results.data.token);
+            window.localStorage.setItem('token', results.data.token);
+            navigate('/home');
         } else {
-            return
+            console.log(results.error.message)
         }
     }
     return (
-        <form onSubmit={(event) =>{
-            event.preventDefault()
-            handleSubmit();
-        }}>
-            <input
-            type= 'text'
-            placeholder='Enter Username'
-            onChange={(event) =>setUsername(event.target.value)}/>
-            <input
-            type='password'
-            placeholder='Enter Password'
-            onChange={(event) =>setPassword(event.target.value)}/>
-            <button id='button'
-            type='submit'>Submit</button>
-        </form>
+        <div id="loginForm">
+      <h1 id="loginHead">Log In</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button type="submit">Log In</button>
+        <Link to={"/register"}>Sign up</Link>
+      </form>
+    </div>
     )
 }
 
