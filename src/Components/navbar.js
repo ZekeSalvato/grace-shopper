9f5b19ab65818f60d01d42f6946a5a19b34718d8
zekesalvato <zsalvato71@gmail.com>
2022-12-01 16:05:16 -0600

    basic nav styling

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,27 +1,57 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import {
+    AppBar,
+    Toolbar,
+    CssBaseline,
+    Typography,
+    Grid,
+    Button,
+} from '@mui/material';
 
-const Navbar = ({logout, token}) => {
-    return(
-        <header>
-            <nav className="navbar">
-                <Link to= '/'>Home</Link>
-                <Link to= '/profile'>Profile</Link>
-                <Link to= '/products'>Products</Link>
-                <Link to= '/cart'>Cart</Link>
-                
-                { token ? (
-                    <Link to= '/home' onClick={() => logout()}>Logout</Link>
-                ): (
-                    <>
-                    <Link to= '/login'>Login</Link>
-                    <Link to= '/register'>Register</Link>
-                    </>
-                )
-                    
-}
-            </nav>
-        </header>
+const Navbar = ({ logout, token }) => {
+    return (
+        <AppBar
+            position='static'
+            style={{}}>
+            <CssBaseline>
+                <Toolbar>
+                    <Grid container>
+                        <Typography
+                            type="title"
+                            style={{
+                                fontSize: '1.75rem',
+
+                            }}>
+                            Grace Shopper
+                        </Typography>
+                    </Grid>
+                    <Grid container
+                        display="flex"
+                        justifyContent="flex-end"
+                        alignItems="flex-end">
+                        <Typography>
+                        <Link to='/'>Home</Link>
+
+                            <Link to='/profile'>Profile</Link>
+                            <Link to='/products'>Products</Link>
+                            <Link to='/cart'>Cart</Link>
+
+                            {token ? (
+                                <Link to='/home' onClick={() => logout()}>Logout</Link>
+                            ) : (
+                                <>
+                                    <Link to='/login'>Login</Link>
+                                    <Link to='/register'>Register</Link>
+                                </>
+                            )
+
+                            }            </Typography>
+
+                    </Grid>
+                </Toolbar>
+            </CssBaseline>
+        </AppBar>
     )
 }
 
