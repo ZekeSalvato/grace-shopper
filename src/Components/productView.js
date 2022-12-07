9622fe0a02c680c1750ab5bd70f4e12bc450b06c
Stephanie Pierce <stephanie.jane.pierce@gmail.com>
2022-12-07 03:13:22 -0600

    more styling to products

--- src/Components/productView.js
+++ src/Components/productView.js
@@ -1,41 +1,36 @@
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { getProducts } from "../api";
-<<<<<<< HEAD
 import './CSS/productView.css';
-=======
 import { addToCart } from '../api';
->>>>>>> refs/remotes/origin/main
 
-const ProductView = ({ products, user, navigate }) => {
+function ProductView({ products, user, navigate }) {
     const { productId } = useParams();
 
     if (products.length) {
         const [currentProduct] = products.filter(product => product.id == productId);
         const { title, description, price } = currentProduct;
         const { isAdmin, id } = user;
-        
+
         return (
-        <div className = 'productViewDiv'>
-            <div>
-             
-                <h3>{title}</h3>
-                <p className="description">Description: {description}</p>
-                <p>Price: {price}</p>
-                <button onClick = { async (event) => { addToCart()}}>Add to Cart</button>
-                <Link to='/products'>Back</Link>
-            </div>
-            {
-                isAdmin ? (
+            <div className='productViewDiv'>
+                <div>
+
+                    <h3>{title}</h3>
+                    <p className="description">Description: {description}</p>
+                    <p>Price: {price}</p>
+                    <button onClick={async (event) => { addToCart(); } }>Add to Cart</button>
+                    <Link to='/products'>Back</Link>
+                </div>
+                {isAdmin ? (
                     <Link to={`/products/edit/${productId}`}>Edit Product</Link>
                 ) : (
                     <p></p>
-                )
-            }
-        </div>
-    )
+                )}
+            </div>
+        );
     } else {
-        <h1>Waiting for product..</h1>
+        <h1>Waiting for product..</h1>;
     }
 }
 
